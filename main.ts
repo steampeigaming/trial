1a96d5f35a873fa497498f8e176facbbcacc5949
steampeigaming <143010608+steampeigaming@users.noreply.github.com>
2023-08-23 12:55:38 -0300

    Music

--- main.ts
+++ main.ts
@@ -7,6 +7,7 @@ for (let index = 0; index < 4; index++) {
         . . # . .
         `)
 }
+music.play(music.stringPlayable("- E D D G G B - ", 120), music.PlaybackMode.UntilDone)
 basic.forever(function () {
 	
 })
